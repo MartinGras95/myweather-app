16ec5213d86b9471e33eecb9573405205e169f1c
Martin Gras <martin.gras95@gmail.com>
2020-01-08 21:56:43 +0000

    api call fix

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends React.Component {
         const country = e.target.elements.country.value;
 
         // Api call to URL to get weather data
-        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`); 
+        const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`); 
         const data = await api_call.json();
 
         // If city and country have been entered
