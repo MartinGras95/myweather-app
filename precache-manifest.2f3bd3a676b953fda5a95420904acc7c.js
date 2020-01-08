f803227a90f2e201716d401c92bd5cbb47fa55fa
Martin Gras <martin.gras95@gmail.com>
2020-01-08 21:47:52 +0000

    Updates

---
+++ precache-manifest.2f3bd3a676b953fda5a95420904acc7c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2c43e66ecdaa6d9712fdfb0a671d12b",
+    "url": "/myweather-app/index.html"
+  },
+  {
+    "revision": "079c9b319aa136c37e25",
+    "url": "/myweather-app/static/css/2.7181f94e.chunk.css"
+  },
+  {
+    "revision": "8de0387c0de80c2a6ba3",
+    "url": "/myweather-app/static/css/main.3cf3cafc.chunk.css"
+  },
+  {
+    "revision": "079c9b319aa136c37e25",
+    "url": "/myweather-app/static/js/2.6dbe4c46.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/myweather-app/static/js/2.6dbe4c46.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8de0387c0de80c2a6ba3",
+    "url": "/myweather-app/static/js/main.bc75ee65.chunk.js"
+  },
+  {
+    "revision": "cf40f9bbaf71f739f06e",
+    "url": "/myweather-app/static/js/runtime-main.93c30043.js"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/myweather-app/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/myweather-app/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/myweather-app/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/myweather-app/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/myweather-app/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/myweather-app/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
